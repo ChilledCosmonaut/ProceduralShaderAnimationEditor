514a2d08a1295750428f270d39a44f56f58afd32
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2023-03-12 16:29:34 +0100

    tried to add some transformation

--- Assets/ProceduralShaderAnimation/Editor/ShaderAnimationGeneratorEditor.cs
+++ Assets/ProceduralShaderAnimation/Editor/ShaderAnimationGeneratorEditor.cs
@@ -14,6 +14,7 @@ namespace ProceduralShaderAnimation.Editor
         private AnimationData animationData;
         [SerializeField] 
         private GroupInfo currentActiveGroup;
+        private Bounds objectBounds;
 
         private readonly List<(BoxBoundsHandle, RectangularWeight)> boxWeights = new ();
 
@@ -25,6 +26,7 @@ namespace ProceduralShaderAnimation.Editor
             generator = (ShaderAnimationGenerator) target;
             animationData = generator.animationData;
             currentActiveGroup = animationData.groupInfos[0];
+            objectBounds = generator.GetComponent<MeshFilter>().sharedMesh.bounds;
             SetupGizmos();
         }
 
@@ -52,7 +54,10 @@ namespace ProceduralShaderAnimation.Editor
         private void OnSceneGUI()
         {
             if (!debug) return;
+            Handles.color = Color.red;
+            Handles.DrawWireCube(objectBounds.center, objectBounds.extents * 2);
 
+            Handles.color = Color.white;
             foreach (FunctionData typelessWeight in currentActiveGroup.weightInfos)
             {
                 switch (typelessWeight)
@@ -91,8 +96,8 @@ namespace ProceduralShaderAnimation.Editor
         {
             var newBoundHandle = new BoxBoundsHandle
             {
-                center = weightInfo.origin - Vector3.one,
-                size = weightInfo.diameters
+                center = TransformIntoWorldSpace(weightInfo.origin),
+                size = TransformIntoWorldSpace(weightInfo.diameters)
             };
             
             boxWeights.Add((newBoundHandle, weightInfo));
@@ -100,17 +105,17 @@ namespace ProceduralShaderAnimation.Editor
         
         private void DrawBoxHandle(BoxBoundsHandle boxHandle, RectangularWeight weightInfo)
         {
-            boxHandle.center = weightInfo.origin;
-            boxHandle.size = weightInfo.diameters;
+            boxHandle.center = TransformIntoWorldSpace(weightInfo.origin);
+            boxHandle.size = TransformIntoWorldSpace(weightInfo.diameters);
             
             EditorGUI.BeginChangeCheck();
-            Vector3 newOrigin = Handles.PositionHandle(weightInfo.origin, generator.transform.rotation);
+            Vector3 newOrigin = Handles.PositionHandle(boxHandle.center, generator.transform.rotation);
             boxHandle.DrawHandle();
             if (!EditorGUI.EndChangeCheck()) return;
             
             Undo.RecordObject(animationData, "Changed Box Weight");
-            weightInfo.origin = newOrigin;
-            weightInfo.diameters = boxHandle.size;
+            weightInfo.origin = TransformIntoBoundingSpace(newOrigin);
+            weightInfo.diameters = TransformIntoBoundingSpace(boxHandle.size);
             recalculate = true;
         }
         
@@ -119,33 +124,56 @@ namespace ProceduralShaderAnimation.Editor
             var rotation = generator.transform.rotation;
             
             EditorGUI.BeginChangeCheck();
-            Vector3 newOrigin = Handles.PositionHandle(weightInfo.origin, rotation);
-            float scale = Handles.RadiusHandle(rotation, weightInfo.origin, weightInfo.radius);
+            Vector3 newOrigin = Handles.PositionHandle(TransformIntoWorldSpace(weightInfo.origin), rotation);
+            float scale = Handles.RadiusHandle(rotation, TransformIntoWorldSpace(weightInfo.origin), weightInfo.radius * objectBounds.extents.x);
             if (!EditorGUI.EndChangeCheck()) return;
             
             Undo.RecordObject(animationData, "Changed Sphere Weight");
-            weightInfo.origin = newOrigin;
-            weightInfo.radius = scale;
+            weightInfo.origin = TransformIntoBoundingSpace(newOrigin);
+            weightInfo.radius = scale / objectBounds.extents.x;
             recalculate = true;
         }
 
         private void DrawInterpolationHandle(InterpolationData weightInfo)
         {
             var rotation = generator.transform.rotation;
+
+            var transformedFirstPoint = TransformIntoWorldSpace(weightInfo.firstControlPoint);
+            var transformedSecondPoint = TransformIntoWorldSpace(weightInfo.firstControlPoint);
             
-            Handles.SphereHandleCap(0, weightInfo.firstControlPoint, rotation, 0.2f, EventType.Repaint);
-            Handles.SphereHandleCap(0, weightInfo.secondControlPoint, rotation, 0.15f, EventType.Repaint);
-            Handles.DrawLine(weightInfo.firstControlPoint, weightInfo.secondControlPoint, 7);
+            Handles.SphereHandleCap(0, transformedFirstPoint, rotation, 0.2f, EventType.Repaint);
+            Handles.SphereHandleCap(0, transformedSecondPoint, rotation, 0.15f, EventType.Repaint);
+            Handles.DrawLine(transformedFirstPoint, transformedSecondPoint, 7);
             
             EditorGUI.BeginChangeCheck();
-            Vector3 firstPoint = Handles.PositionHandle(weightInfo.firstControlPoint, rotation);
-            Vector3 secondPoint = Handles.PositionHandle(weightInfo.secondControlPoint, rotation);
+            Vector3 firstPoint = Handles.PositionHandle(transformedFirstPoint, rotation);
+            Vector3 secondPoint = Handles.PositionHandle(transformedSecondPoint, rotation);
             if (!EditorGUI.EndChangeCheck()) return;
             
             Undo.RecordObject(animationData, "Changed Spline Control Points");
-            weightInfo.firstControlPoint = firstPoint;
-            weightInfo.secondControlPoint = secondPoint;
+            weightInfo.firstControlPoint = TransformIntoBoundingSpace(firstPoint);
+            weightInfo.secondControlPoint = TransformIntoBoundingSpace(secondPoint);
             recalculate = true;
+        }
+
+        private Vector3 TransformIntoBoundingSpace(Vector3 position)
+        {
+            return ScaleToBoundingSpace(position) - objectBounds.extents + objectBounds.center;
+        }
+        
+        private Vector3 TransformIntoWorldSpace(Vector3 position)
+        {
+            return ScaleToWorldSpace(position) - objectBounds.extents + objectBounds.center;
+        }
+        
+        private Vector3 ScaleToBoundingSpace(Vector3 position)
+        {
+            return new Vector3(position.x / objectBounds.extents.x, position.y / objectBounds.extents.y, position.z / objectBounds.extents.z);
+        }
+        
+        private Vector3 ScaleToWorldSpace(Vector3 position)
+        {
+            return new Vector3(position.x * objectBounds.extents.x, position.y * objectBounds.extents.y, position.z * objectBounds.extents.z);
         }
     }
 }
